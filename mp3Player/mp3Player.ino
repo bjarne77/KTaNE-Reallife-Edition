ee8af65aa41971a4f3b7fc96b7b7311fa7cf0841
Steffen Clemens <info@steffenclemens.de>
2025-04-12 00:34:15 +0200

    Tick tick tick boom

--- mp3Player/mp3Player.ino
+++ mp3Player/mp3Player.ino
@@ -13,10 +13,32 @@ void setup()
 
   player.begin(FPSerial, /*isACK = */true, /*doReset = */true);
 
+  // set volume between 0 and 30
   player.volume(20);
-  player.play(1);
 }
 
 void loop()
 {
+  static unsigned long timerTick = millis();
+  static unsigned long timerExplodeStart = millis();
+  static unsigned long timerExplodeStop = millis();
+  static bool explode = false;
+
+  if (millis() - timerExplodeStart > 12000) {
+    explode = true;
+    timerExplodeStart = millis();
+    timerExplodeStop = millis();
+    // play explosion sound
+    player.play(2);
+  }
+
+  if (millis() - timerExplodeStop > 5000) {
+    explode = false;
+  }
+
+  if (!explode && millis() - timerTick > 1000) {
+    timerTick = millis();
+    // play tick sound
+    player.play(1);
+  }
 }
